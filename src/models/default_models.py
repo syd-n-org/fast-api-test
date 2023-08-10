32071bdd8a010e05343d4f2319fca4d9787b062d
Sidharth Narayan <sidharth_narayan@outlook.com>
2023-08-10 10:21:36 +0530

    revamped project

--- src/models/default_models.py
+++ src/models/default_models.py
@@ -1,17 +1,14 @@
 from pydantic import BaseModel
 from typing import Any, Optional
-
 # from src.db.connections import db
-
 
 class ResponseModel(BaseModel):
     content: Any
     response_code: int = 200
     message: Optional[str]
 
-
 class RecordModel(BaseModel):
-    pet_id: int  # = list(db.get_collection("ANIMAL_ALL").find({}).sort(key_or_list="pet_id",direction=-1))[0]
-    name: str
-    animal: str
-    breed: str
+    pet_id  : int #= list(db.get_collection("ANIMAL_ALL").find({}).sort(key_or_list="pet_id",direction=-1))[0]
+    name    : str
+    animal  : str
+    breed   : str
